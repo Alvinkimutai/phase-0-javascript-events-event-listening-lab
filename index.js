4e739bd57adb53b602086727e3ebd462e5c8ff4e
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-07-20 05:50:08 +0300

    starts on event listners

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const main = document.querySelector('#button')
+
 function addingEventListener() {
+  return main.addEventListener('click', () => alert('Event listener'));
 }
+
+addingEventListener()
